22023f583f54915c9ea36842d870b609e9400748
tjcjc <tjcjc@qq.com>
2019-07-12 18:27:08 +0800

    Initial commit

---
+++ Example/JTUtils/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JTUtils
+//
+//  Created by tjcjc on 07/12/2019.
+//  Copyright (c) 2019 tjcjc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
