f9daa04b472a36ab5631604627958cab17a3c91b
tjcjc <taijcjc@gmail.com>
2019-09-03 03:51:40 +0800

    test tag branch

--- .travis/publish.rb
+++ .travis/publish.rb
@@ -1,5 +1,5 @@
 puts ENV["TRAVIS_BRANCH"]
-if ENV["TRAVIS_BRANCH"] == "release"
+if ENV["TRAVIS_BRANCH"].match /^([\d.])+$/
     system("which pod")
     system("echo 'begin publish'")
     system("pod repo add Jason https://github.com/tjcjc/Specs")
@@ -7,5 +7,5 @@ if ENV["TRAVIS_BRANCH"] == "release"
     system "/usr/local/bin/git -C /Users/travis/.cocoapods/repos/Jason remote add origin https://tjcjc:$PUBLISH_KEY@github.com/tjcjc/Specs.git"
     system "/usr/local/bin/git -C /Users/travis/.cocoapods/repos/Jason fetch"
     system "/usr/local/bin/git -C /Users/travis/.cocoapods/repos/Jason branch --set-upstream-to=origin/master master"
-    system "pod repo push Jason JTUtils.podspec --allow-warnings"
+    system "pod repo push Jason JTUtils.podspec --allow-warnings --verbose"
 end
--- JTUtils.podspec
+++ JTUtils.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'JTUtils'
-  s.version          = '0.1.8'
+  s.version          = '0.1.9'
   s.summary          = 'Some useful code for myself.'
 
 # This description is used to generate tags and improve search results.
