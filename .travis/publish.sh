630f60b02ab2c5ff8a73dcfb0501d20e5aff8d63
tjcjc <taijcjc@gmail.com>
2019-09-03 02:50:06 +0800

    update bash file

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
     pod repo add Jason https://github.com/tjcjc/Specs
     /usr/local/bin/git -C /Users/travis/.cocoapods/repos/Jason remote remove origin
     /usr/local/bin/git -C /Users/travis/.cocoapods/repos/Jason remote add origin https://tjcjc:$PUBLISH_KEY@github.com/tjcjc/Specs.git
